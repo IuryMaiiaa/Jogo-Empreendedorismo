22ddbe6a8914585ba967652fd7f8c78f544b9b78
Neils01 <iurryy@yahoo.com.br>
2016-03-07 19:21:36 -0300

    adicionando prototipo da interface do armazem

--- Assets/ScriptAjustancoCamera.cs
+++ Assets/ScriptAjustancoCamera.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class ScriptAjustancoCamera : MonoBehaviour {
     public Camera camera;
+    public Transform mainCameraPosition;
+    public Transform armazemCameraPosition;
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +15,14 @@ public class ScriptAjustancoCamera : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    public void moveCameraArmazem()
+    {
+        camera.transform.position = armazemCameraPosition.position;
+    }
+
+    public void moveCameraMainPosition()
+    {
+        camera.transform.position = mainCameraPosition.position;
+    }
 }
