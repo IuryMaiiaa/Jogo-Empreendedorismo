04ff1715a4e5dee987b7286f08973d8c39fabb53
Neils01 <iurryy@yahoo.com.br>
2016-03-12 19:57:37 -0300

    adicionando metodos ao armazenamento
    adicionado salvamento dos dados do armazem, integrado ao sistema.

---
+++ Assets/ArmazemGerenciamento.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmazemGerenciamento : MonoBehaviour {
+    public int recursoPlantaArmazenado;
+    public int recursoMelecaArmazenado;
+    public int recursoCouroArmazenado;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void plantAdicionar(int valor)
+    {
+        recursoPlantaArmazenado += valor;
+    }
+
+    public void melecaAdicionar(int valor)
+    {
+        recursoMelecaArmazenado += valor;
+    }
+
+    public void couroAdicionar(int valor)
+    {
+        recursoCouroArmazenado += valor;
+    }
+
+    public void plantaRemover(int valor)
+    {
+        recursoPlantaArmazenado -= valor;
+    }
+
+    public void melecaRemover(int valor)
+    {
+        recursoMelecaArmazenado -= valor;
+    }
+
+    public void couroRemover(int valor)
+    {
+        recursoCouroArmazenado -= valor;
+    }
+
+    public void save()
+    {
+
+    }
+
+    public void load()
+    {
+
+    }
+
+}
+
+[System.Serializable]
+class ArmazenamentoData
+{
+
+    public int recursoPlantaArmazenado;
+    public int recursoMelecaArmazenado;
+    public int recursoCouroArmazenado;
+}
