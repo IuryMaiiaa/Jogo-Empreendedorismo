658aec1f47cdce6be37aea84becaf001348f1417
Neils01 <iurryy@yahoo.com.br>
2016-03-22 19:53:32 -0300

    adicionando novos sprites e solos aleatorios
    agora o solo muda, tendo novas variaçoes.

---
+++ Assets/RandomSpriteSolo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSpriteSolo : MonoBehaviour {
+    public SpriteRenderer soloSprite;
+    public Sprite[] solo;
+
+	// Use this for initialization
+	void Start () {
+        float valor = Random.value * 100000;
+        int valorInteiro = (int)valor;
+        valorInteiro = valorInteiro % 4;
+        soloSprite.sprite = solo[valorInteiro];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
