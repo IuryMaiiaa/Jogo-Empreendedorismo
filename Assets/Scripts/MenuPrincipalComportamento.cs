f6b0ad88774df2fbfe8290ab983b6d50df4d736e
Neils01 <iurryy@yahoo.com.br>
2016-04-05 12:01:11 -0300

    adicionei novo menu iniciar de acordo com o desing

---
+++ Assets/Scripts/MenuPrincipalComportamento.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuPrincipalComportamento : MonoBehaviour {
+    public GameObject[] BotoesReferencia;
+    public int curentIndex;
+    public GameObject naveSprite;
+
+	// Use this for initialization
+	void Start () {
+        curentIndex = 0;
+        //naveSprite.transform.position = BotoesReferencia[curentIndex].transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown("down"))
+        {
+            indexValueDown();
+        } else if(Input.GetKeyDown("up"))
+        {
+            indexValueUp();
+        }
+        
+	
+	}
+
+    public void indexValueDown()
+    {
+        if((curentIndex+1) < BotoesReferencia.Length)
+        {
+            curentIndex++;
+            naveSprite.transform.position = BotoesReferencia[curentIndex].transform.position;
+        }
+    }
+
+    public void indexValueUp()
+    {
+        if ((curentIndex - 1) >= 0)
+        {
+            curentIndex--;
+            naveSprite.transform.position = BotoesReferencia[curentIndex].transform.position;
+        }
+    }
+}
