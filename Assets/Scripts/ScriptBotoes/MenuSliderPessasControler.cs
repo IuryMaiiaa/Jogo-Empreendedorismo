bee670768c694c96cc685ce7e139072562ce835f
Neils01 <iurryy@yahoo.com.br>
2016-05-12 14:55:32 -0300

    corrigindo posição das pastas

---
+++ Assets/Scripts/ScriptBotoes/MenuSliderPessasControler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSliderPessasControler : MonoBehaviour {
+    public Animator sliderAnimacao;
+
+	// Use this for initialization
+	void Start () {
+        sliderAnimacao.SetBool("BotaoPrecionado",false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
